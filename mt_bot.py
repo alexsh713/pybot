6b3d1e42c99dfb44a40e11120d462e56e4ea955a
alexsh713 <pe4orin713@gmail.com>
2017-12-04 23:48:50 +0300

    coin api

--- mt_bot.py
+++ mt_bot.py
@@ -1,26 +1,22 @@
 #-*- coding: utf-8 -*-
 import config
 import telebot
-from lxml import html
 import requests
+import auth
 from time import sleep
 from bittrex import Bittrex
-import auth
+from requests.exceptions import ConnectionError
+from time import sleep
+
 bot = telebot.TeleBot(config.token)
-url = 'https://coinmarketcap.com/'
-zcash = ".//*[@id='id-zcash']/td[4]/a"
-btc = ".//*[@id='id-bitcoin']/td[4]/a"
 
 
-def coin_price(coin):
-	
-	result = requests.get(url)
-	tree = html.fromstring(result.text)
-	value = tree.xpath(coin)
-	for i in value:
-		coin_val = int(i.text.split('.')[0].split('$')[1])
-	
-	return coin_val
+
+def extract_arg(arg):
+    try:
+        return arg.split()[1]
+    except IndexError:
+        return "Не, ну а че?"
 		
 
 
@@ -33,17 +29,21 @@ def handle_start_help(message):
 	
 
 
-
 @bot.message_handler(commands=['zcash'])
 def handle_zchash(message):
-    bot.send_message(message.chat.id, coin_price(zcash))
-
+    zcash = requests.get('https://api.coinmarketcap.com/v1/ticker/zcash/')
+    output = zcash.json()[0]['price_usd'] + "$" + "       " + zcash.json()[0]['percent_change_24h'] + "%"
+    bot.send_message(message.chat.id, output)
 
 
 
 @bot.message_handler(commands=['bitcoin'])
 def handle_btc(message):
-    bot.send_message(message.chat.id, coin_price(btc))
+    btc = requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
+    output = btc.json()[0]['price_usd'] + "$" + "       " + btc.json()[0]['percent_change_24h'] + "%"
+    bot.send_message(message.chat.id, output)
+
+
 
 @bot.message_handler(commands=['bittrex'])
 def handle_bittrex_balance(message):
@@ -55,60 +55,33 @@ def handle_bittrex_balance(message):
 
 @bot.message_handler(commands=['test'])
 def test(message):
-    for i in range(1, 4):
-        bot.send_message(message.chat.id, i)
-        sleep(30)
+    arg=extract_arg(message.text)
+    bot.send_message(message.chat.id, arg.upper())
+
+
+
+@bot.message_handler(commands=['check'])
+def check_katka(message):
+    i = 0
+    while True:
+        try:
+            r = requests.get('http://192.168.12.181:42000/getstat')
+        
+        except ConnectionError:
+            bot.send_message(message.chat.id, "Катка подохла")
+
+        i+=1
+        sleep(5)
+
+        if i > 5:
+            break
+
+
+
 
 @bot.message_handler(content_types=["text"])
 def handle_other(message):
 	bot.send_message(message.chat.id, "А, понял")
-
-
-
-
-
-
-
-
-# @bot.message_handler(content_types=["text"])
-# def repeat_all_messages(message):
-	
-# 	if message.text == "z":
-# 		bot.send_message(message.chat.id, coin_price(zcash))
-
-	
-# 	elif message.text == "btc":
-# 		bot.send_message(message.chat.id, coin_price(btc))
-	
-# 	elif message.text == "card":
-# 		try:
-# 			r = requests.get(url1)
-# 			data  = r.json()
-# 			dict0 =  data['result'][0]
-# 			dict1 =  data['result'][1]
-# 			dict2 =  data['result'][2]
-#                         dict3 =  data['result'][3]
-# 			mes1 = "card0_speed: " + str(dict0['speed_sps']) + ' ' +  "card0_temp: " + str(dict0['temperature'])
-# 			mes2 = "card1_speed: " + str(dict1['speed_sps']) + ' ' +  "card1_temp: " + str(dict1['temperature'])
-# 			mes3 = "card2_speed: " + str(dict2['speed_sps']) + ' ' +  "card2_temp: " + str(dict2['temperature'])
-#                         mes4 = "card3_speed: " + str(dict3['speed_sps']) + ' ' +  "card2_temp: " + str(dict3['temperature'])
-# 			all_mes = mes1 + "\n" + mes2 + "\n" + mes3 +"\n" + mes4
-# 			bot.send_message(message.chat.id, all_mes)
-# 		except requests.exceptions.ConnectionError:
-# 			bot.send_message(message.chat.id, "не отвечает эта хуета")
-	
-# 	elif message.text == "bittrex":
-# 		my_b = Bittrex(auth.api_key, auth.api_secret)
-# 		balance = my_b.get_balance('ZEC')
-# 		zec_value = balance['result']['Available']
-# 		bot.send_message(message.chat.id, zec_value)
-
-
-
-# 	else:
-# 		bot.send_message(message.chat.id, "пошел нахуй, не знаю такой команды")
-
-
 
 
 
@@ -120,8 +93,4 @@ if __name__ == '__main__':
 
 
 
-
-# my_b = Bittrex(auth.api_key, auth.api_secret)
-# balance = my_b.get_balance('ZEC')
-# zec_value = balance['result']['Available']
 
